e3b530b513211bc2da1896ba2027d6106d50bd07
Zahra1993Norouzi <zahranoroozi@outlook.com>
2024-11-05 22:59:35 +0330

    Create read_from_memory.c

---
+++ read_from_memory.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+
+int main() {
+    key_t key = ftok("shmfile", 65);
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+
+    char *str = (char*) shmat(shmid, NULL, 0);  
+
+    printf("Data read from memory: %s\n", str);
+
+    shmdt(str);  
+    return 0;
+}
